adf8078230ddf00e6c27b9559918b461c3303468
ITSON <ITSON@alumnoSW-04.itson.edu.mx>
2023-08-10 17:19:18 -0700

    dashboardapp config

--- Proyecto_Flores_Gamez/urls.py
+++ Proyecto_Flores_Gamez/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from Proyecto_Flores_Gamez.views import saludo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("dashboard/", include("dashboard.urls")),
     path('saludo/', saludo),
 ]
