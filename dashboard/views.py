f8a4cfc9a4ac7658c95e96cfb93637d8a38f57b2
Ana Gamez <ana.gamez.elizalde@gmail.com>
2023-08-26 11:53:34 -0700

    plotly

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import plotly.graph_objs as go
+import plotly.offline as opy
+from django.shortcuts import render
+import plotly.express as px
+
+
 
 # Create your views here.
 def index(request):
     return HttpResponse("holaaaaaaaa")
 
 def dashboard(request):
-    context = {
- 
-    }
-    return render(request, "dashboard.html", context)
+    df = px.data.stocks()
+    fig = px.line(df, x='date', y="GOOG")
+    
+    # Convierte el gráfico a HTML
+    plot_html = fig.to_html(full_html=False, default_height=500)
+    
+    return render(request, 'dashboard.html', {'plot_html': plot_html})
